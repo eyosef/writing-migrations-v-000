ce737ab18836a6ef9b4ef10be5b3e2f9094dc9f0
Eden Mesfun <emesfun@gmail.com>
2018-03-16 14:38:00 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class Add_grade_and_birthday_to_Students < ActiveRecord::Migration
 
   def change
-    add_column :students,
+    add_column :students, :grade, integer, :birthdate, string
   end
 end
