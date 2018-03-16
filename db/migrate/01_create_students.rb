30d15bf68f5833619fae94122f7165a48cb299c7
Eden Mesfun <emesfun@gmail.com>
2018-03-16 14:33:11 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
 def change
-  create table :students do |column|
+  create_table :students do |column|
     column.string :name
   end #do iteration
 end
