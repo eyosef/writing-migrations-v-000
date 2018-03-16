7d596b3cffa6205c17699bcce49f394fa8c51450
Eden Mesfun <emesfun@gmail.com>
2018-03-16 14:47:32 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
-    change_column(:students, :birthdate, :string)
+    change_column(:students, :birthdate, :datetime)
   end
 end
