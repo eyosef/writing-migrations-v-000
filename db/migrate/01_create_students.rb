c6309648c989000c0cb85e893a318f4252325f29
Eden Mesfun <emesfun@gmail.com>
2018-03-16 14:26:55 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,4 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
-  connection = ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
+
 end
